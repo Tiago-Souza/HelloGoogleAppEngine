f131248e08be624fdefb7b8bbafa6b3257cdba40
Tiago Souza <tiagosouza@Tiagos-MacBook-Air.local>
2018-12-20 12:56:41 -0500

    Initial commit

---
+++ src/main/java/com/example/HelloGoogleAppEngine/HelloGoogleAppEngineApplication.java
@@ -0,0 +1,14 @@
+package com.example.HelloGoogleAppEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGoogleAppEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGoogleAppEngineApplication.class, args);
+	}
+
+}
+
